1314a911c2f01a65f99bf7063b9d16804e95ea97
Frode Aannevik <frode.aa@gmail.com>
2024-08-05 23:38:10 +0200

    Fix jest WARN about ts-jest config under global
    The current config is already the default preset for ts-jest
    
    Rel: https://kulshekhar.github.io/ts-jest/docs/getting-started/presets#advanced

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,5 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest', {
-      tsconfig: 'tsconfig.json',
-    }],
-  },
-  testMatch: ['**/test/**/*.test.(ts|js)'],
-  globals: {
-    'ts-jest': {
-        tsconfig: 'tsconfig.json',
-    }
-  }
+  testMatch: ['**/test/**/*.test.(ts|js)']
 };
